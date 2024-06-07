12be0f661a33c647fb30f9f99664d302198e6c96
Muhammad Rizky Ferdiansyah <103484286+rizkyfrdiansyah@users.noreply.github.com>
2024-06-07 15:52:32 +0700

    Develop toggle todo feature

--- todo-apps2/src/components/TodoItem.jsx
+++ todo-apps2/src/components/TodoItem.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, toggleCompleted }) => {
+  // Menerima function toggleCompleted sebagai sebuah prop
+  const getTodoTitleStyle = () => {
+    if (todo.completed === true) {
+      return {textDecoration: 'line-through'}
+    } else {
+      return {textDecoration: 'none'}
+    }
+  }
+
+  // Definisikan function getTodoTitleStyle di sini
+  // const getTodoTitleStyle = () => {
+  //   if (todo.completed === true) {
+  //     return {textDecoration: 'line-through'}
+  //   } else {
+  //     return {textDecoration: 'none'}
+  //   }
+  // }
+
+  // Definisikan function toggleCompleted di sini
+  // const toggleCompleted = () => {
+  //   console.log('toggleCompleted function is called')
+  // }
+
+
+  // Code dibawah adalah error!
+  // const todoTitleStyle = {
+  //   if (todo.completed === true) {
+  //     textDecoration: 'line-through'
+  //   } else {
+  //     textDecoration: 'none'
+  //   }
+  // }
+
     return (
       <div style={styles.todoItem}>
-        <p>{todo.title}</p>
+        {/* Menambahkan checkbox */}
+        <input 
+        type="checkbox" 
+        style={styles.checkbox}
+        //  Memberikan id dari todo sebagai argument
+        // Jalankan function toggleCompleted ketika checkbox di-klik
+        onChange={() => toggleCompleted(todo.id)}
+        />
+        {/* Panggil function getTodoTitleStyle */}
+        <p style={getTodoTitleStyle()}>{todo.title}</p>
       </div>
     )
   }
@@ -12,6 +54,16 @@ const TodoItem = ({ todo }) => {
     todoItem: {
       border: '2px solid #000',
       fontSize: '24px',
+      // Tambahkan styles di bawah ini
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    // Tambahkan styles di bawah ini
+    checkbox: {
+      marginRight: '10px',
+      height: '18px',
+      width: '18px',
     },
   }
   
