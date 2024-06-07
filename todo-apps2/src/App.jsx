7b4e375c760d488aeec7e3da3b29e33adeadd94e
Muhammad Rizky Ferdiansyah <103484286+rizkyfrdiansyah@users.noreply.github.com>
2024-06-07 16:34:07 +0700

    Develop add todo feature

--- todo-apps2/src/App.jsx
+++ todo-apps2/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import Todos from './components/Todos'
+import TodoForm from './components/TodoForm'
 
 function App() {
   const [todos, setTodos] = useState([
     {
       id: 1,
       title: 'Finish Progate React Course',
-      completed: false,
+      completed: false, // Menganti nilai awal ke true
     },
     {
       id: 2,
@@ -22,14 +23,53 @@ function App() {
 
   console.log(todos)
 
+  // Definisikan toggleCompleted di sini
+  const toggleCompleted = (todoId) => {
+    const upatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(upatedTodos)
+    
+    console.log('toggleCompleted function is called')
+    console.log(todoId)
+  }
+
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId)
+    setTodos(updatedTodos)
+
+    console.log('deleteTodo function is called')
+    console.log(todoId)
+  }
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+
+    const updatedTodos = todos.concat(newTodo)
+    setTodos(updatedTodos)
+    console.log('This is addTodo Function is')
+  }
+  
+
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      {/* Gunakan method map di sini */}
-      {/* {todos.map((todo) => {
-        return <p key={todo.id}>{todo.title}</p>
-      })} */}
-      <Todos todos={todos} /> {/* Menampilkan component Todos */}
+      <TodoForm addTodo={addTodo} />
+      <Todos 
+        todos={todos} 
+        toggleCompleted={toggleCompleted} 
+        deleteTodo={deleteTodo} /> {/* Menampilkan component Todos */}
     </div>
   )
 }
